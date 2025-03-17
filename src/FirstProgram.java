ba45cfd75e15fdbc9d69474757fc153ddab3fa9d
Hari <harinarayanan363@gmail.com>
2025-03-17 22:36:13 +0530

    my second commit

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -3,5 +3,7 @@ public class FirstProgram {
 	public static void main(String[] args) {
 		
 		System.out.println("My first program");
+		System.out.println("My Second program");
+		System.out.println("My Third program");
 	}
 }
